7a91686d9f03633cd0c4e99190445e3e66133b42
Pierrick <pierrickcurt@gmail.com>
2022-07-31 20:13:40 +0200

    wip

--- recipes-mapio/mapio-init/mapio-init/mapio-init.sh
+++ recipes-mapio/mapio-init/mapio-init/mapio-init.sh
@@ -3,5 +3,9 @@
 # Resize data partiton to maximal size
 resize2fs /dev/mmcblk1p4
 
+# Create volume directories for containers
+mkdir -p /usr/local/domoticz
+mkdir -p /usr/local/homebridge
+
 # Signal that first boot has been done
 touch /boot/first_boot_done
